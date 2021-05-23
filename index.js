e6bcfe2bb59132b7f0ac6148d998871090ce91c1
Luiz Henrique <58401291+MogLuiz@users.noreply.github.com>
2021-05-23 17:02:46 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,11 +3,22 @@ var app = express()
 var http = require("http").createServer(app)
 var io = require("socket.io")(http)
 
+
+//Abrindo evento de conexão
+io.on("connection",(socket) => {
+    console.log(socket)
+    console.log(socket.id)
+})
+
+
 app.set("view engine","ejs")
 
 app.get("/", (req, res) => {
     res.render("index")
 })
+
+
+
 
 http.listen(3000, () => {
     console.log("Server running")
